d0d28a8c80031961e9189ce287ba932033173482
Jing <jianruj2@illinois.edu>
2024-10-01 19:28:41 -0500

    lab 3 commented

--- workspace/Lab3/Lab3.c
+++ workspace/Lab3/Lab3.c
@@ -1,5 +1,5 @@
 //#############################################################################
-// FILE:   LABstarter_main.c
+// FILE:   LAB 3.c
 //
 // TITLE:  Lab Starter
 //#############################################################################
@@ -26,7 +26,7 @@
 // The Launchpad's CPU Frequency set to 200 you should not change this value
 #define LAUNCHPAD_CPU_FREQUENCY 200
 
-
+//JS add definations for note period values and SONG_LENGTH
 #define C4NOTE ((uint16_t)(((50000000/2)/2)/261.63))
 #define D4NOTE ((uint16_t)(((50000000/2)/2)/293.66))
 #define E4NOTE ((uint16_t)(((50000000/2)/2)/329.63))
@@ -48,6 +48,7 @@
 #define A5FLATNOTE ((uint16_t)(((50000000/2)/2)/830.61))
 #define OFFNOTE 0
 #define SONG_LENGTH 61
+//JS songarray for short happy birthday
 //uint16_t songarray[SONG_LENGTH] = {
 //E4NOTE,
 //OFFNOTE,
@@ -97,6 +98,7 @@
 //A4NOTE,
 //A4NOTE,
 //A4NOTE};
+//JS songarray for mary had a little lamb
 uint16_t songarray[SONG_LENGTH] = {
 E4NOTE,
 OFFNOTE,
@@ -172,9 +174,13 @@ uint32_t numSWIcalls = 0;
 extern uint32_t numRXA;
 uint16_t UARTPrint = 0;
 uint16_t LEDdisplaynum = 0;
+//JS define a global 16 bit variable to trigger increase or decrease of duty cycle
 int16_t updown = 1;
+//JS define a global 16 bit variable for duty cycle
 int16_t mycount = 0;
+//JS define a global float variable for motor control effort and servo angle
 float motorcount = 0.0;
+//JS define a global 16 bit variable for songarray index
 int16_t songindex = 0;
 
 void main(void)
@@ -196,15 +202,22 @@ void main(void)
     GpioDataRegs.GPBSET.bit.GPIO34 = 1;
 
 	// LED1 and PWM Pin
+    //JS PinMux to set GPIO22 to EPWM12A
     GPIO_SetupPinMux(22, GPIO_MUX_CPU1, 5);
     //GPIO_SetupPinOptions(22, GPIO_OUTPUT, GPIO_PUSHPULL);
     //GpioDataRegs.GPACLEAR.bit.GPIO22 = 1;
 	
+    //JS PinMux to set GPIO2 to EPWM2A
     GPIO_SetupPinMux(2, GPIO_MUX_CPU1, 1);
+    //JS PinMux to set GPIO3 to EPWM2B
     GPIO_SetupPinMux(3, GPIO_MUX_CPU1, 1);
+    //JS PinMux to set GPIO14 to EPWM8A
     GPIO_SetupPinMux(14, GPIO_MUX_CPU1, 1);
+    //JS PinMux to set GPIO15 to EPWM8B
     GPIO_SetupPinMux(15, GPIO_MUX_CPU1, 1);
+    //JS PinMux to set GPIO16 to EPWM9A
     GPIO_SetupPinMux(16, GPIO_MUX_CPU1, 5);
+    //JS PinMux to set GPIO104 to SCITXDD for UARTD’s transmit
     GPIO_SetupPinMux(104, GPIO_MUX_CPU1, 6);
 	// LED2
     GPIO_SetupPinMux(94, GPIO_MUX_CPU1, 0);
@@ -390,6 +403,7 @@ void main(void)
     // Configure CPU-Timer 0, 1, and 2 to interrupt every given period:
     // 200MHz CPU Freq,                       Period (in uSeconds)
     ConfigCpuTimer(&CpuTimer0, LAUNCHPAD_CPU_FREQUENCY, 10000);
+    //JS for exercise 4, change to play happy birthday
     ConfigCpuTimer(&CpuTimer1, LAUNCHPAD_CPU_FREQUENCY, 125000);
     ConfigCpuTimer(&CpuTimer2, LAUNCHPAD_CPU_FREQUENCY, 10000);
 
@@ -399,61 +413,81 @@ void main(void)
     CpuTimer2Regs.TCR.all = 0x4000;
 
 	init_serialSCIA(&SerialA,115200);
-
+	//same for the following EPWM, all EPWM settings are the same as EPWM12, unless specified
+	//JS Count up Mode bit is 00
     EPwm12Regs.TBCTL.bit.CTRMODE = 0;
-    //JS 2/3
+    //JS 2/3 free run bit is 1x, 10 or 11
     EPwm12Regs.TBCTL.bit.FREE_SOFT = 2;
+    //JS disable the phase loading bit is 0
     EPwm12Regs.TBCTL.bit.PHSEN = 0;
+    //JS CLKDIV is 1, 2 to the power of 0
     EPwm12Regs.TBCTL.bit.CLKDIV = 0;
+    //JS Start the timer at 0
     EPwm12Regs.TBCTR = 0;
+    //JS Signal needs to be 20KHz, to have a period of 50 microseconds, TBPRD value get divided by carrier frequency
     EPwm12Regs.TBPRD = 2500;
+    //JS 0%*TBPRD for duty cycle
     EPwm12Regs.CMPA.bit.CMPA = 0;
+    //JS when TBCTR=CMPA, clear to set to low
     EPwm12Regs.AQCTLA.bit.CAU = 1;
+    //JS when TBCTR =0,set it to high
     EPwm12Regs.AQCTLA.bit.ZRO = 2;
+    //JS copy from guideline
     EPwm12Regs.TBPHS.bit.TBPHS = 0;
 
+
     EPwm2Regs.TBCTL.bit.CTRMODE = 0;
-    //JS 2/3
     EPwm2Regs.TBCTL.bit.FREE_SOFT = 2;
     EPwm2Regs.TBCTL.bit.PHSEN = 0;
     EPwm2Regs.TBCTL.bit.CLKDIV = 0;
     EPwm2Regs.TBCTR = 0;
     EPwm2Regs.TBPRD = 2500;
     EPwm2Regs.CMPA.bit.CMPA = 0;
+    //JS needs CMPB for EPWM2B
     EPwm2Regs.CMPB.bit.CMPB = 0;
     EPwm2Regs.AQCTLA.bit.CAU = 1;
+    //JS needs CBU for EPWM2B
     EPwm2Regs.AQCTLB.bit.CBU = 1;
     EPwm2Regs.AQCTLA.bit.ZRO = 2;
+    //JS needs AQCTLB.ZRO for EPWM2B
     EPwm2Regs.AQCTLB.bit.ZRO = 2;
     EPwm2Regs.TBPHS.bit.TBPHS = 0;
 
     EPwm8Regs.TBCTL.bit.CTRMODE = 0;
-    //JS 2/3
     EPwm8Regs.TBCTL.bit.FREE_SOFT = 2;
     EPwm8Regs.TBCTL.bit.PHSEN = 0;
+    //JS for exercise 3, to set the carrier frequency to 50Hz. Firstly, divide the orginal by 2^4
     EPwm8Regs.TBCTL.bit.CLKDIV = 4;
     EPwm8Regs.TBCTR = 0;
+    //JS Secondly, to get the period divide TBPRD by carrier frequency value after CLKDIV
     EPwm8Regs.TBPRD = 65535;
     EPwm8Regs.CMPA.bit.CMPA = 0;
+    //JS needs CMPB for EPWM8B
     EPwm8Regs.CMPB.bit.CMPB = 0;
     EPwm8Regs.AQCTLA.bit.CAU = 1;
+    //JS needs CBU for EPWM8B
     EPwm8Regs.AQCTLB.bit.CBU = 1;
     EPwm8Regs.AQCTLA.bit.ZRO = 2;
+    //JS needs AQCTLB.ZRO for EPWM8B
     EPwm8Regs.AQCTLB.bit.ZRO = 2;
     EPwm8Regs.TBPHS.bit.TBPHS = 0;
 
     EPwm9Regs.TBCTL.bit.CTRMODE = 0;
     //JS 2/3
     EPwm9Regs.TBCTL.bit.FREE_SOFT = 2;
+
     EPwm9Regs.TBCTL.bit.PHSEN = 0;
     EPwm9Regs.TBCTL.bit.CLKDIV = 1;
     EPwm9Regs.TBCTR = 0;
     EPwm9Regs.TBPRD = 2500;
+    //JS no need for CMPA
     //EPwm9Regs.CMPA.bit.CMPA = 0;
+    //JS CAU and ZRO value create square wave
     EPwm9Regs.AQCTLA.bit.CAU = 0;
     EPwm9Regs.AQCTLA.bit.ZRO = 3;
     EPwm9Regs.TBPHS.bit.TBPHS = 0;
 
+    //JS add following codes based on guidelines
     EALLOW; // Below are protected registers
     GpioCtrlRegs.GPAPUD.bit.GPIO2 = 1; // For EPWM2A
     GpioCtrlRegs.GPAPUD.bit.GPIO3 = 1; // For EPWM2B
@@ -480,6 +514,7 @@ void main(void)
     PieCtrlRegs.PIEIER12.bit.INTx9 = 1;
 	
 	init_serialSCIC(&SerialC,115200);
+	// JS uncomment for UART demo
 	//init_serialSCID(&SerialD,115200);
     // Enable global Interrupts and higher priority real-time debug events
     EINT;  // Enable Global interrupt INTM
@@ -496,7 +531,8 @@ void main(void)
         }
     }
 }
-
+//JS create function setEPWM2A
+//JS saturate controleffort between -10 to 10 for right motor
 void setEPWM2A(float controleffort)
 {
     if (controleffort > 10){
@@ -505,8 +541,10 @@ void setEPWM2A(float controleffort)
     if (controleffort < -10){
         controleffort = -10;
     }
+    //JS control duty cycle to 0% when controleffort is -10, 50% when controleffort is 0, 100% when controleffort is 10, converts controleffort value between -10 and 10 to duty cycle 0% to 100%
     EPwm2Regs.CMPA.bit.CMPA = ((controleffort + (float)10))/ ((float)20)* EPwm2Regs.TBPRD;
 }
+//JS saturate controleffort between -10 to 10 for left motor
 void setEPWM2B(float controleffort){
     if (controleffort > 10){
         controleffort = 10;
@@ -514,8 +552,10 @@ void setEPWM2B(float controleffort){
     if (controleffort < -10){
         controleffort = -10;
     }
+    //JS control duty cycle to 0% when controleffort is -10, 50% when controleffort is 0, 100% when controleffort is 10, converts controleffort value between -10 and 10 to duty cycle 0% to 100%
     EPwm2Regs.CMPB.bit.CMPB = ((controleffort + (float)10))/ ((float)20)* EPwm2Regs.TBPRD;
 }
+//JS saturate angle between -90 and 90 for RCServo1
 void setEPWM8A_RCServo(float angle)
 {
     if (angle > 90){
@@ -524,8 +564,10 @@ void setEPWM8A_RCServo(float angle)
     if (angle < -90){
         angle = -90;
     }
+    //JS control duty cycle to 4% when angle is -90, 8% when angle is 0, 12% when angle is 90, converts angle value between -90 and 90 to duty cycle 4% to 12%
     EPwm8Regs.CMPA.bit.CMPA = ((((float)8/(float)180)*angle+(float)8)/100)* EPwm8Regs.TBPRD;
 }
+//JS saturate angle between -90 and 90 for RCServo2
 void setEPWM8B_RCServo(float angle)
 {
     if (angle > 90){
@@ -534,6 +576,7 @@ void setEPWM8B_RCServo(float angle)
     if (angle < -90){
         angle = -90;
     }
+    //JS control duty cycle to 4% when angle is -90, 8% when angle is 0, 12% when angle is 90, converts angle value between -90 and 90 to duty cycle 4% to 12%
     EPwm8Regs.CMPB.bit.CMPB = ((((float)8/(float)180)*angle+(float)8)/100)* EPwm8Regs.TBPRD;
 }
 // SWI_isr,  Using this interrupt as a Software started interrupt
@@ -587,12 +630,16 @@ __interrupt void cpu_timer0_isr(void)
 
 // cpu_timer1_isr - CPU Timer1 ISR
 __interrupt void cpu_timer1_isr(void)
+
 {
     if (songindex >= SONG_LENGTH){
+        //JS when songindex get to the SONG_LENGTH, change the mux of pin to GPIO16 from EPWM9A
         GPIO_SetupPinMux(16, GPIO_MUX_CPU1, 0);
+        //JS set GPIO16 to low
         GpioDataRegs.GPACLEAR.bit.GPIO16 = 1;
     }
     if (songindex<SONG_LENGTH){
+        //JS when songindex less than SONG_LENGTH, set TBPRD to the period of note in songarray, and increase songindex
         EPwm9Regs.TBPRD = songarray[songindex];
         songindex++;
     }
@@ -605,39 +652,50 @@ __interrupt void cpu_timer1_isr(void)
 // cpu_timer2_isr CPU Timer2 ISR
 __interrupt void cpu_timer2_isr(void)
 {
-
+//JS comment the code below used for exercise 1 and 2
+//jS when updown is 1, the value set to the duty cycle mycount and controleffort value motorcount increases by 1 and 0.1 respectively until the duty cycle is 100% or motorcount reaches 10
 //    if (updown == 1){
 //        mycount++;
 //        motorcount+=0.1;
+    //JS pass motorcount to setEPWM2A to run the right motor
 //        setEPWM2A(motorcount);
+    //JS pass motorcount to setEPWM2B to run the left motor
 //        setEPWM2B(motorcount);
 //
 //	    EPwm12Regs.CMPA.bit.CMPA = mycount;
+    //JS when motorcount is larger or equal to 10,set updowm to 0 to start decreasing motorcount
 //	    if (motorcount >= 10.0){
 //	        updown =0;
 //	    }
-////        if (mycount == EPwm12Regs.TBPRD){
-////            updown = 0;
-////        }
+    //JS when duty cycle value equal to TBPRD, set updown to 0 to decrease the duty cycle mycount
+//        if (mycount == EPwm12Regs.TBPRD){
+//            updown = 0;
+//        }
 //	}
 //    else{
 //        mycount--;
 //        motorcount-=0.1;
+    //JS pass motorcount to setEPWM2A to run the right motor
 //        setEPWM2A(motorcount);
+    //JS pass motorcount to setEPWM2B to run the left motor
 //        setEPWM2B(motorcount);
 //
 //        EPwm12Regs.CMPA.bit.CMPA = mycount;
+    //JS when motorcount is less or equal to -10,set updowm to 1 to start increasing motorcount
 //        if (motorcount <= -10.0){
 //            updown = 1;
 //        }
+    //JS when duty cycle equals to 0, set updown to 1 to increase the duty cycle mycount
 //        if (mycount == 0){
 //            updown = 1;
 //        }
 //	}
 
+    //JS for exercise 3, copied code from exercise 1 and 2 to change the angle of servo
     if (updown == 1){
             mycount++;
             motorcount+=0.1;
+            //JS pass motorcount to setEPWM8A_RCServo and setEPWM8B_RCServo
             setEPWM8A_RCServo(motorcount);
             setEPWM8B_RCServo(motorcount);
             EPwm12Regs.CMPA.bit.CMPA = mycount;
@@ -651,6 +709,7 @@ __interrupt void cpu_timer2_isr(void)
         else{
             mycount--;
             motorcount-=0.1;
+            //JS pass motorcount to setEPWM8A_RCServo and setEPWM8B_RCServo
             setEPWM8A_RCServo(motorcount);
             setEPWM8B_RCServo(motorcount);
             EPwm12Regs.CMPA.bit.CMPA = mycount;
